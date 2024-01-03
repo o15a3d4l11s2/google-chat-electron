ff48fd76b961d8bc38f15f9d249e638e8eb938b5
Dimitar Topalov <o15a3d4l11s2@gmail.com>
2024-01-03 12:34:00 +0200

    Updated badge icon

--- src/main/features/badgeIcon.ts
+++ src/main/features/badgeIcon.ts
@@ -8,10 +8,10 @@ type IconTypes = 'offline' | 'normal' | 'badge';
 const decideIcon = (href: string): IconTypes => {
   let type: IconTypes = 'offline';
 
-  if (href.match(/favicon_chat_r2/) ||
-    href.match(/favicon_chat_new_non_notif_r2/)) {
+  if (href.match(/favicon_chat_r3/) ||
+    href.match(/favicon_chat_new_non_notif_r3/)) {
     type = 'normal';
-  } else if (href.match(/favicon_chat_new_notif_r2/)) {
+  } else if (href.match(/favicon_chat_new_notif_r3/)) {
     type = 'badge';
   }
 
